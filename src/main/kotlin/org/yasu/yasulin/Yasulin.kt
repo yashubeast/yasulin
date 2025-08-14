bfd0620e199a32ef18fa1a22f6263a8263a2960f
yashubeast <shenziouminami@gmail.com>
2025-08-14 20:02:42 +0530

    initial commit

---
+++ src/main/kotlin/org/yasu/yasulin/Yasulin.kt
@@ -0,0 +1,14 @@
+package org.yasu.yasulin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Yasulin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
